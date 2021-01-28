2d85498c624c0b41b8cf433059133e54517ac900
Dirk Jäger <dirk.jaeger@schufa.de>
2021-01-28 12:00:33 +0100

    include build in deploy script

--- deploy-to-artifactory.sh
+++ deploy-to-artifactory.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+echo "Build linux and windows binary of image-tools"
+go build
+GOOS=windows GOARCH=amd64 go build
+
+echo "Push to artifactory"
 curl -ujaegdi:AP5guU7a8TN9S6zAtrRadGyMBUt -T image-tools     "https://artifactory-pro.sf-rz.de:8443/artifactory/scpas-bin-dev-local/istag_and_image_management/image-tools-linux/image-tools"
 curl -ujaegdi:AP5guU7a8TN9S6zAtrRadGyMBUt -T image-tools.exe "https://artifactory-pro.sf-rz.de:8443/artifactory/scpas-bin-dev-local/istag_and_image_management/image-tools-windows/image-tools.exe"
