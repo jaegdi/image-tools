b5e362720f0a5ea785baf8fb2ec0c50f21ac0d03
Dirk Jäger <dirk.jaeger@schufa.de>
2022-06-08 11:10:43 +0200

    disable profiler

--- main.go
+++ main.go
@@ -3,13 +3,7 @@ package main
 import (
 	"fmt"
 	. "image-tools/ocrequest"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"os"
-	"runtime"
-	"runtime/pprof"
-	"sync"
+	// _ "net/http/pprof"
 )
 
 func init() {
@@ -17,31 +11,33 @@ func init() {
 }
 
 func main() {
-	// --%<---- Profiling
-	var wg sync.WaitGroup
-	if CmdParams.Options.Profiler {
-		f, perr := os.Create("cpu.pprof")
-		if perr != nil {
-			log.Fatal(perr)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-		go func() {
-			runtime.SetBlockProfileRate(1)
-			InfoLogger.Println(http.ListenAndServe("localhost:6060", nil))
-		}()
-	}
-	// --%<---- Profiling
+	// // --%<---- Begin Profiling
+	// var wg sync.WaitGroup
+	// if CmdParams.Options.Profiler {
+	// 	f, perr := os.Create("cpu.pprof")
+	// 	if perr != nil {
+	// 		log.Fatal(perr)
+	// 	}
+	// 	pprof.StartCPUProfile(f)
+	// 	defer pprof.StopCPUProfile()
+	// 	go func() {
+	// 		runtime.SetBlockProfileRate(1)
+	// 		InfoLogger.Println(http.ListenAndServe("localhost:6060", nil))
+	// 	}()
+	// }
+	// // --%<---- End Profiling
+
 	if CmdParams.Options.ServerMode {
 		fmt.Println("Will be startet in server mode")
 
 	} else {
 		CmdlineMode()
 	}
-	// --%<---- Profiling
-	if CmdParams.Options.Profiler {
-		wg.Add(1)
-		wg.Wait()
-	}
-	// --%<---- Profiling
+
+	// // --%<---- Begin Profiling
+	// if CmdParams.Options.Profiler {
+	// 	wg.Add(1)
+	// 	wg.Wait()
+	// }
+	// // --%<---- End Profiling
 }
